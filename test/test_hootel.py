9f03711b61fc75d6a566dd8138ef9fa1448c6cfb
Triber <triberdaniel@gmail.com>
2025-04-14 18:24:33 +0200

    resolution change

--- test/test_hootel.py
+++ test/test_hootel.py
@@ -1,5 +1,5 @@
 import time
-from telnetlib import EC
+from selenium.webdriver.support import expected_conditions as EC
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -16,6 +16,7 @@ class TestHootel(object):
         options.add_experimental_option("detach", True)
         options.add_argument('--headless')
         self.browser = webdriver.Chrome(options=options)
+        self.browser.maximize_window()
         self.browser.get(URL)
 
     def teardown_method(self):
@@ -26,9 +27,10 @@ class TestHootel(object):
     @allure.severity(allure.severity_level.TRIVIAL)
     @allure.tag("login")
     def test_login(self):
+        print(self.browser.get_window_size())
         time.sleep(1)
         # login_btn = self.browser.find_element(By.XPATH, '//a[@class="nav-link"]')
-        login_btn = WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.XPATH, '//a[@class="nav-link"]')))
+        login_btn = WebDriverWait(self.browser, 5).until(EC.element_to_be_clickable((By.XPATH, '//a[@class="nav-link"]')))
         time.sleep(1)
         login_btn.click()
 
